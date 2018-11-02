3ab2fd21583eb7c03cdb0f7ce01207f8a189bfd9
Katdemit <nguyenducvutruong@gmail.com>
2018-11-02 20:24:22 +0700

    Changed a bit

---
+++ Twin Sticks 2.5D/Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    Vector3 offset;
+    GameObject ball;
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.FindGameObjectWithTag("Player");
+        offset = transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = ball.transform.position + offset;
+	}
+}
